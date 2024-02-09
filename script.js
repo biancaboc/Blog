029f19b84b848c17fb57315ffc23e35104f0eea6
biancaboc <159582175+biancaboc@users.noreply.github.com>
2024-02-09 22:36:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openPassions() {
+    window.location.href = 'index.html';
+}
+
+function openTravels() {
+    window.location.href = 'travels.html';
+}
+
+function openQuestions() {
+    window.location.href = 'questions.html';
+}
